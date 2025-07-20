cee9ebfd4f39ced15aeebc41a3b0be1a7631d7e2
marinagvmp <143155143+marinagvmp@users.noreply.github.com>
2025-07-20 00:29:13 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Home from './Home';
 import Ecommerce from './Ecommerce';
 import Checkout from './Checkout';
@@ -7,22 +8,23 @@ import Confirmation from './Confirmation';
 import Cart from './Cart';
 import Banner from './Banner';
 import Footer from './Footer';
+
 import { CartProvider } from './CartContext';
 
 function App() {
   return (
     <CartProvider>
-        <Router>
-          <Banner />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/ecommerce" element={<Ecommerce />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/confirmation" element={<Confirmation />} />
-            <Route path="/cart" element={<Cart />} />
-          </Routes>
-          <Footer />
-        </Router>
+      <Router basename="/magasin">
+        <Banner />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/ecommerce" element={<Ecommerce />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/confirmation" element={<Confirmation />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+        <Footer />
+      </Router>
     </CartProvider>
   );
 }
